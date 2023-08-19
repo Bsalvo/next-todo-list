d18a4619e2b5293abf3187c4556853df54d32fb5
Bsalvo <bruno95bh@gmail.com>
2023-08-18 23:22:23 -0300

    SCHEMA para tablea client

--- src/app/lib/validation/todo.tsx
+++ src/app/lib/validation/todo.tsx
@@ -5,9 +5,3 @@ export const TodoSchema = z.object({
     title: z.string().min(1, 'Tarefa não pode estar vazia'),
     complete: z.boolean().optional()
 })
-
-export const ClientSchema = z.object({
-    id: z.string().optional(),
-    name: z.string().min(3, 'Nome precisa ser maior que 3 letras'),
-    document: z.string()
-})
